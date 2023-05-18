bada55fb34d595aa0c7a770751f3ca4121941fd0
Robert Santos <robertsantosti@outlook.com>
2023-05-18 20:51:02 -0300

    Concluindo exercicio-01

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+
+function Button(props) {
+  return(
+    <div>
+      <button>{props.label}</button>
+    </div>
+  )
+}
+
+export default Button;
---
+++ src/components/Button.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import Button from './Button';
+
+/*
+ * [M3S03] EX01 - Criar um componente 'Botão' que receba uma label utilizando TDD
+*/
+
+describe('Testando o componente Button', () => {
+  test('Renderizando o botão na tela', () => {
+    render(<Button/>);
+    const buttonHtmlElement = screen.getByRole('button');
+    expect(buttonHtmlElement).toBeInTheDocument();
+  });
+
+  test(`Exibindo label 'Meu botão' no componente <Button/>`, () => {
+    render(<Button label='Meu botão'/>);
+
+    const labelText = screen.queryByText('Meu botão');
+    expect(labelText).toBeInTheDocument();
+  })
+});
